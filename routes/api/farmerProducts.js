f948fa0fda004e0835d19f2ad6fd8a8b70b5499d
Santu-Hansariafood <santu@hansariafood.com>
2024-06-20 13:12:54 +0530

    error remove

--- routes/api/farmerProducts.js
+++ routes/api/farmerProducts.js
@@ -2,19 +2,14 @@ const express = require('express');
 const router = express.Router();
 const FarmerProductController = require('../../controllers/FarmerProductController');
 
-// GET all farmer products
 router.get('/', FarmerProductController.getAllFarmerProducts);
 
-// GET a specific farmer product by ID
 router.get('/:id', FarmerProductController.getFarmerProduct, FarmerProductController.getFarmerProductById);
 
-// POST a new farmer product
 router.post('/', FarmerProductController.createFarmerProduct);
 
-// PUT update a farmer product
 router.put('/:id', FarmerProductController.getFarmerProduct, FarmerProductController.updateFarmerProduct);
 
-// DELETE a farmer product
 router.delete('/:id', FarmerProductController.getFarmerProduct, FarmerProductController.deleteFarmerProduct);
 
 module.exports = router;
