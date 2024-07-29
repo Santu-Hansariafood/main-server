56e613efc48656a573c84a258daaf3b51dca5671
Santu-Hansariafood <santu@hansariafood.com>
2024-07-29 12:05:47 +0530

    added the bill

--- models/Counter.js
+++ models/Counter.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const counterSchema = new mongoose.Schema({
   id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
+  seq: { type: Number, default: 0 },
 });
 
-const Counter = mongoose.model('Counter', counterSchema);
+const Counter = mongoose.model("Counter", counterSchema);
 
 module.exports = Counter;
