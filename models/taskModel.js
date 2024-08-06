f2eb2f0a09e3f429bed9d067fda77b351cd24131
Santu-Hansariafood <santu@hansariafood.com>
2024-08-06 17:09:30 +0530

    task and bills api updated

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,31 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  taskName: {
-    type: String,
-    required: true,
+const taskSchema = new mongoose.Schema(
+  {
+    taskName: {
+      type: String,
+      required: true,
+    },
+    taskDescription: {
+      type: String,
+      required: true,
+    },
+    assignTo: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+      enum: ["High", "Medium", "Low"],
+    },
+    status: {
+      type: String,
+      default: "Pending",
+      enum: ["Pending", "Accepted", "Complete"],
+    },
+    feedback: {
+      type: String,
+    },
   },
-  taskDescription: {
-    type: String,
-    required: true,
-  },
-  assignTo: {
-    type: String,
-    required: true,
-  },
-  priority: {
-    type: String,
-    required: true,
-    enum: ['High', 'Medium', 'Low'],
-  },
-  status: {
-    type: String,
-    default: 'Pending',
-    enum: ['Pending', 'Accepted', 'Complete'],
-  },
-  feedback: {
-    type: String,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Task', taskSchema);
+module.exports = mongoose.model("Task", taskSchema);
