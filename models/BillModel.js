e1a6cc30f39c4e2f41f52202e1c6c4d278680f23
Santu-Hansariafood <santu@hansariafood.com>
2024-08-10 11:57:12 +0530

    change the date formate

--- models/BillModel.js
+++ models/BillModel.js
@@ -45,13 +45,13 @@ const billSchema = new mongoose.Schema({
   selectedGodownName: { type: String, required: true },
   date: {
     type: String,
-    default: () => {
-      const date = new Date();
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
-    },
+    // default: () => {
+    //   const date = new Date();
+    //   const day = String(date.getDate()).padStart(2, '0');
+    //   const month = String(date.getMonth() + 1).padStart(2, '0');
+    //   const year = date.getFullYear();
+    //   return `${day}/${month}/${year}`;
+    // },
   },
 });
 
