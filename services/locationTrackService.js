206269ca4b7a0040996b1335f7a5497ab8b582ef
Santu-Hansariafood <santu@hansariafood.com>
2024-09-21 11:19:41 +0530

    added the controller

--- services/locationTrackService.js
+++ services/locationTrackService.js
@@ -1,17 +1,30 @@
-// services/locationTrackService.js
-
-const LocationTrack = require('../models/LocationTrack');
+const LocationTrack = require("../models/LocationTrack");
 
 const addLocationTrack = async (employeeId, lat, lng) => {
+  const lastLocation = await LocationTrack.findOne({ employeeId }).sort({
+    timestamp: -1,
+  });
+
+  const currentTime = new Date();
+
+  if (lastLocation) {
+    const lastLocationTime = new Date(lastLocation.timestamp);
+
+    const timeDifference = (currentTime - lastLocationTime) / (1000 * 60 * 60);
+    if (timeDifference < 24) {
+      lastLocation.coordinates.lat = lat;
+      lastLocation.coordinates.lng = lng;
+      return await lastLocation.save();
+    }
+  }
+
   const newLocation = new LocationTrack({
     employeeId,
     coordinates: { lat, lng },
+    timestamp: currentTime,
   });
-  return await newLocation.save();
-};
 
-const getLocationByEmployee = async (employeeId) => {
-  return await LocationTrack.find({ employeeId }).sort({ timestamp: -1 });
+  return await newLocation.save();
 };
 
 module.exports = {
